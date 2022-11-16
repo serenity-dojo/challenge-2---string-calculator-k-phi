8e99b7907deaeb42b80d3173155f45974be9c683
k-phi <kraft.philipp@gmail.com>
2022-11-16 21:00:32 +0100

    Do exercise

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -1,7 +1,46 @@
 package com.serenitydojo.calculator;
 
+
 public class Calculator {
-    public int evaluate(String expression) {
-        return 0;
+    public int evaluate(String expression) throws IllegalMathsOperatorException {
+        int left = 0;
+        if (!expression.isEmpty()) {
+            String[] components = expression.split(" ");
+
+            int right;
+            String operator = "+";
+
+            for (String component : components) {
+                try {
+                    right = Integer.parseUnsignedInt(component);
+                    left = calculate(left, operator, right);
+                } catch (NumberFormatException e) {
+                    operator = component;
+                }
+            }
+        }
+        return left;
+    }
+
+    private int calculate(int left, String operation, int right) throws IllegalMathsOperatorException {
+        int result;
+        switch (operation) {
+            case "+": {
+                result = left + right;
+                break;
+            }
+            case "-": {
+                result = left - right;
+                break;
+            }
+            case "*": {
+                result = left * right;
+                break;
+            }
+            default: {
+                throw new IllegalMathsOperatorException("Operation " + operation);
+            }
+        }
+        return result;
     }
 }
---
+++ src/main/java/com/serenitydojo/calculator/IllegalMathsOperatorException.java
@@ -0,0 +1,7 @@
+package com.serenitydojo.calculator;
+
+public class IllegalMathsOperatorException extends Exception {
+    public IllegalMathsOperatorException(String message) {
+        super(message);
+    }
+}
